b016979f00cd41ed2de4ad00e0627a65c74abf3e
lucag316 <lucafranco316@gmail.com>
2025-06-04 18:53:07 -0300

    Entorno actualizado e instalado

--- red-social-back/src/app.module.ts
+++ red-social-back/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+//import { AppController } from './app.controller';
+//import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config'; // lo agrego el profe
+import { MongooseModule } from '@nestjs/mongoose'; // lo agrego el profe
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler'; // lo agrego el profe
+import { APP_GUARD } from '@nestjs/core'; // lo agrego el profe
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // configuracion global
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    // configuracion mongo
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      uri: ConfigService.get<string>('DATABASE_URL'),
+      retryWrites: true,
+      w: 'majority',
+    })
+  ],
 })
 export class AppModule {}
