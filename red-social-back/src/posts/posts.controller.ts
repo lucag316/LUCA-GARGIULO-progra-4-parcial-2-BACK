57f2417417f0314900a2c69a744c03d1deb4b7bc
lucag316 <lucafranco316@gmail.com>
2025-06-22 21:51:11 -0300

    Agregando el controller y el module de posts

---
+++ red-social-back/src/posts/posts.controller.ts
@@ -0,0 +1,58 @@
+import { Controller, Post, Body, Get, Delete, Param, Query, Req, UseGuards, UseInterceptors, UploadedFile } from "@nestjs/common";
+import { PostService } from "./post.service";
+import { CreatePostDto } from "./dto/create-post.dto";
+import { GetPostDto } from "./dto/get-post.dto";
+//import { AuthGuard } from "@nestjs/passport";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { FileValidationPipe } from "src/common/pipes/file-validation.pipe";
+import { Request } from "express";
+
+@Controller('posts')
+//@UseGuards(AuthGuard('jwt'))
+export class PostController {
+    constructor(private readonly postService: PostService) {}
+
+    // crear publicacion
+    @Post()
+    @UseInterceptors(FileInterceptor('imagenPerfil', { dest: './uploads' }))
+    async create(
+        @Body() createPostDto: CreatePostDto, 
+        @Req() req: Request,
+        @UploadedFile(new FileValidationPipe()) file?: Express.Multer.File, 
+    ) {
+        const imagenUrl = file ? `uploads/${file.filename}` : undefined;
+        const user = req.user as { sub: string };
+        const userId = user.sub;
+        return this.postService.create(createPostDto, userId, imagenUrl);
+    }
+
+    // Listar publicaciones con filtros
+    @Get()
+    async findAll(@Query() getPostDto: GetPostDto) {
+        return this.postService.findAll(getPostDto);
+    }
+
+    // Baja lógica
+    @Delete(':id')
+    async delete(@Param('id') id: string, @Req() req: Request) {
+        const user = req.user as { sub: string };
+        const userId = user.sub;
+        return this.postService.softDelete(id, userId);
+    }
+
+    // Dar me gusta
+    @Post(':id/me-gusta')
+    async like(@Param('id') postId: string, @Req() req: Request) {
+        const user = req.user as { sub: string };
+        const userId = user.sub;
+        return this.postService.addLike(postId, userId);
+    }
+
+    // Quitar me gusta
+    @Delete(':id/me-gusta')
+    async unlike(@Param('id') postId: string, @Req() req: Request) {
+        const user = req.user as { sub: string };
+        const userId = user.sub;
+        return this.postService.removeLike(postId, userId);
+    }
+}
--- red-social-back/src/posts/posts.module.ts
+++ red-social-back/src/posts/posts.module.ts
@@ -1,2 +1,20 @@
 
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PostController } from './posts.controller';
+import { PostService } from './post.service';
+import { Post, PostSchema } from './schemas/post.schema';
+import { UsersModule } from 'src/users/users.module';
+import { Mongoose } from 'mongoose';
+
+
+@Module({
+    imports: [
+        MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]),
+        UsersModule
+    ],
+    controllers: [PostController],
+    providers: [PostService],
+})
+
 export class PostsModule {}
